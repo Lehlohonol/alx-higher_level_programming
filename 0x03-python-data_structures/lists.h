de306503bcb7129f83bc3e661ea56cfd0dd85717
Hloni <pretorius1724@gmail.com>
2024-01-09 09:14:54 -0800

    print done

---
+++ 0x03-python-data_structures/lists.h
@@ -0,0 +1,18 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Data Structures: Lists, Tuples Project
+ */
+
+// size_t print_listint(const listint_t *h);
+
+#endif
